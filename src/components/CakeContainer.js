506b8bafe4563ff904b2fe1788771680fba9d1da
neelxie <kidricederek@gmail.com>
2021-10-09 10:29:35 +0300

    Add fix for cake container

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -2,8 +2,9 @@ import React from "react";
 
 function CakeContainer() {
   return <div>
-    <h1>Cakes</h1>
-  </div>;
+    <h2>Number of Cakes</h2>
+    <button>Buy Cakes</button>
+  </div>
 }
 
 export default CakeContainer;
