a47605883e82ca7bf6d5a517460fe2b2b3963f4e
neelxie <kidricederek@gmail.com>
2021-10-09 10:59:02 +0300

    Add action to component

--- src/components/CakeContainer.js
+++ src/components/CakeContainer.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { connect } from "react-redux";
+import { buyCake } from '../redux'
 
-function CakeContainer() {
+function CakeContainer(props) {
   return <div>
-    <h2>Number of Cakes</h2>
-    <button>Buy Cakes</button>
+    <h2>Number of Cakes-- {props.numberOfCakes}</h2>
+    <button onClick={ () => props.buyCake}>Buy Cakes</button>
   </div>
 }
 
-export default CakeContainer;
+const mapStateToProps = (state) => {
+  return {
+    numberOfCakes: state.numberOfCakes
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    buyCakes: () => dispatch(buyCake())
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps(CakeContainer));
