5d596ea0ec4a38fec1944235b274e19146b8039d
neelxie <kidricederek@gmail.com>
2021-10-16 11:14:51 +0300

    Add new changes

--- src/components/ItemContainer.js
+++ src/components/ItemContainer.js
@@ -1,20 +1,32 @@
-import React from 'react'
-import { connect } from 'react-redux'
+import React from "react";
+import { connect } from "react-redux";
+import buyCake from '../redux/cakes/cakeActions'
+import buyIceCream from '../redux/icecream/icecreamActions'
 
 function ItemContainer(props) {
-
   return (
     <div>
       <h2>Item - {props.item}</h2>
     </div>
-  )
+  );
 }
 
 const mapStateToProps = (state, ownProps) => {
-  const itemState = ownProps.cake ? state.cake.numOfCakes : state.iceCream.numOfIcecreams
+  const itemState = ownProps.cake
+    ? state.cake.numOfCakes
+    : state.iceCream.numOfIcecreams;
   return {
-    item: itemState
-  }
-}
+    item: itemState,
+  };
+};
 
-export default connect(mapStateToProps)(ItemContainer)
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const dispatchFunction = ownProps.cake
+    ? () => dispatch(buyCake())
+    : () => dispatch(buyIceCream());
+  return {
+    buyItem: dispatchFunction,
+  };
+};
+
+export default connect(mapStateToProps)(ItemContainer);
